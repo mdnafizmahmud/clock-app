91c36bcd0bfd5d1c52bc3b1d9938214a86860904
Nafiz <nafizmahmud52@gmail.com>
2026-05-09 00:55:37 +0600

    date add

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-let hoursEl = document.getElementById("hours-num");
-let minutesEl = document.getElementById("minutes-num");
-let secondsEl = document.getElementById("seconds-num");
-let ampmEl = document.getElementById("ampm");
+const hoursEl = document.getElementById("hours-num");
+const minutesEl = document.getElementById("minutes-num");
+const secondsEl = document.getElementById("seconds-num");
+const dateEl = document.getElementById("date");
+const monthEl = document.getElementById("month");
+const yearEl = document.getElementById("year");
+const ampmEl = document.getElementById("ampm");
 
 function startClock() {
   const date = new Date();
-  let h = date.getHours();
-  let m = date.getMinutes();
-  let s = date.getSeconds();
-  let ampm = "AM";
+  let day, month, year, h, m, s, ampm;
+  h = date.getHours();
+  m = date.getMinutes();
+  s = date.getSeconds();
+  day = date.getDate();
+  month = date.getMonth();
+  year = date.getFullYear();
+  ampm = "AM";
 
-  if (h >= 12) {
-    ampm = "PM";
-  } else {
-    ampm = ampm;
-  }
+  ampm = h >= 12 ? "PM" : ampm;
 
   if (h > 12) {
     h -= 12;
@@ -23,10 +26,15 @@ function startClock() {
     h = 12;
   }
 
+  day = day < 10 ? "0" + day : day;
+  month = month < 10 ? "0" + month : month;
   h = h < 10 ? "0" + h : h;
   m = m < 10 ? "0" + m : m;
   s = s < 10 ? "0" + s : s;
 
+  dateEl.textContent = day;
+  monthEl.textContent = month;
+  yearEl.textContent = year;
   hoursEl.textContent = h;
   minutesEl.textContent = m;
   secondsEl.textContent = s;
